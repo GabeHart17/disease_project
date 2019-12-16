ad713ea78a684da326fb5938b35fb12316b86531
Gabe Hart <flyingmessier@gmail.com>
2019-12-16 15:11:09 -0500

    added file

---
+++ part_1.py
@@ -0,0 +1,38 @@
+import matplotlib
+import csv
+
+# starting values
+S = 0
+R = 0
+I = 0
+T = 0
+
+# constants
+r = 0
+m = 0
+k = 0
+
+# simulation params
+delta_t = 1
+t_max = 1
+
+def dS_dt():
+    pass
+
+def dR_dt():
+    pass
+
+def dI_dt():
+    pass
+
+def dT_dt():
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
